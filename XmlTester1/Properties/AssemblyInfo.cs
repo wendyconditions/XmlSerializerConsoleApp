ae6d6f8199abbc3963211527a0179e7139b997f0
Wendy Maldonado <wendymaldonado.la@gmail.com>
2018-01-05 15:17:12 -0800

    No change needed, duh

--- XmlTester1/Properties/AssemblyInfo.cs
+++ XmlTester1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("")]
+[assembly: Guid("9f0d0318-3670-4a69-98aa-af5aaa93597d")]
 
 // Version information for an assembly consists of the following four values:
 //
