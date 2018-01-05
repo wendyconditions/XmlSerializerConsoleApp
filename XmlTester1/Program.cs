4f6e85cfdc8e9748b686712c1aa040a08d220ca6
wendyconditions <wendymaldonado.la@gmail.com>
2018-01-05 15:52:54 -0800

    Wip

--- XmlTester1/Program.cs
+++ XmlTester1/Program.cs
@@ -16,61 +16,29 @@ namespace XmlTester1
         {
             GetByTitleKey();
 
-            //bool isConnected = IsServerConnected("Server=;Database=;");
-            //Console.WriteLine("DB isConnected = {0}", isConnected);
+            Post();
 
-            //if (isConnected)
-            //{
-            //    //ExecuteInsert();
-
-            //    //Post(6);
-
-            //    //ExecuteDelete();
-
-            //    //ExecuteSelect();
-
-            //    //ExecuteSelectById();
-
-                
-            //}
-
-            //Console.ReadLine();
         }
-
-        //private static bool IsServerConnected(string connectionString)
-        //{
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        try
-        //        {
-        //            connection.Open();
-        //            return true;
-        //        }
-        //        catch (SqlException ex)
-        //        {
-        //            Console.WriteLine(ex.Message);
-        //            return false;
-        //        }
-        //    }
-        //}
 
         public static CustomField GetByTitleKey()
         {
-            string xml = "<CustomField><Field name = \"Credits\"> Editor, Chris Wyatt; music, Nathan Wang; historical advisors, Yehuda Bauer, Michael Berenbaum, Mic</Field><Field name = \"Participants\"> Narrator, Anthony Hopkins.</Field><Field name = \"Organization: Creator\">[presented by] Steven Spielberg in association with Survivors of the Shoah Visual History Foundation </Field ></CustomField>";
+            string xml = "<CustomField><Field name = \"Credits\"> Editor, Chris Wyatt; music, Mic</Field><Field name = \"Participants\"> Narrator, Anthony Hopkins.</Field><Field name = \"Organization: Creator\">[presented by] Cooln </Field ></CustomField>";
 
             var item = DeserializeObject<CustomField>(xml);
-            
             
 
             foreach(var prop in item.Fields)
             {
-               
+                Console.WriteLine("=========NAME========");
+                Console.WriteLine(prop.Name);
+                Console.WriteLine("=========FIELD TEXT========");
+                Console.WriteLine(prop.FieldText);
             }
             
             return item;
         }
 
-        public static T DeserializeObject<T>(string xml)
+        private static T DeserializeObject<T>(string xml)
         {
             var serializer = new XmlSerializer(typeof(T));
             using (var tr = new StringReader(xml))
@@ -88,47 +56,51 @@ namespace XmlTester1
 
         public class Field
         {
-            private string attribute_name;
-            private string element_text;
-
             [XmlAttribute("name")]
-            public string Name
-            {
-                get { return attribute_name; }
-                set { attribute_name = value; }
-            }
+            public string Name { get; set; }
 
             [XmlText()]
-            public string FieldText
-            {
-                get { return element_text; }
-                set { element_text = value; }
-            }
+            public string FieldText { get; set; }
         }
 
+        public static void Post()
+        {
+            string xml = "<CustomField><Field name = \"Credits\"> Editor, Chris Wyatt; music, Mic</Field><Field name = \"Participants\"> Narrator, Anthony Hopkins.</Field><Field name = \"Organization: Creator\">[presented by] Cooln </Field ></CustomField>";
 
-        //static void Post(int titleKey, Field field)
-        //{
-        //    var item = SerializeObject(field);
-        //}
+            Field field1 = new Field
+            {
+                Name = "Destination",
+                FieldText = "Oregon Trail"
+            };
 
-        //private static string SerializeObject<T>(T field)
-        //{
-        //    var ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            //add to existing class THEN serialize it and update the db repository, but involves... too much back and forth, rethink
 
-        //    var serializer = new XmlSerializer(typeof(T));
+            var item = SerializeObject(field1);
 
-        //    var settings = new XmlWriterSettings();
-        //    // settings.Indent = false;
-        //    settings.OmitXmlDeclaration = true;
+            //var newElement = new Field();
+            //newElement.Name = 
 
-        //    var stringWriter = new StringWriter();
+            Console.WriteLine("=========Xml========");
+            Console.WriteLine(item);
+        }
 
-        //    using (var writer = XmlWriter.Create(stringWriter, settings))
-        //    {
-        //        serializer.Serialize(writer, field, ns);
-        //        return stringWriter.ToString();
-        //    }
-        //}
+        private static string SerializeObject<T>(T field)
+        {
+            var ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            var settings = new XmlWriterSettings();
+
+            settings.OmitXmlDeclaration = true;
+
+            var stringWriter = new StringWriter();
+
+            using (var writer = XmlWriter.Create(stringWriter, settings))
+            {
+                serializer.Serialize(writer, field, ns);
+                return stringWriter.ToString();
+            }
+        }
     }
 }
